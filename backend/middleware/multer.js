ada6ced27f5b35bec4cafbf99e810383e15cf554
ISHITA172004 <dasishita614@gmail.com>
2025-12-15 17:53:07 +0530

    Update multer.js

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -2,4 +2,4 @@ import multer from "multer";
 
 const storage = multer.memoryStorage(); // store file in memory buffer
 
-export const singleUpload = multer({ storage }).single('file'); // use the storage
+export const singleUpload = multer({ storage }).single('thumbnail'); // use the storage
